cdc58a820e237f930d9415b6ad1ccd60d62bb387
dboissin <damien.boissin@web-education.net>
2015-03-06 17:51:30 +0100

    fix migration script

--- deployment/rss/migration/rss_updateFeeds.js
+++ deployment/rss/migration/rss_updateFeeds.js
@@ -1,12 +1,29 @@
 db.rss.channels.find().forEach(function(rss) {
-	if(rss.feeds && rss.feeds.length > 0){
-		var new_feeds = new Array();
-		var new_feed = new Object();
-		rss.feeds.forEach(function(feed) {
-			new_feed.link = feed;
-			new_feed.show = 5;
-			new_feeds.push(new_feed);
-		});
-		db.rss.channels.update({"_id" : rss._id}, { $set : { "feeds" : new_feeds}});
-	}
+        if(rss.feeds && rss.feeds.length > 0){
+                var new_feeds = new Array();
+                rss.feeds.forEach(function(feed) {
+                        if (feed) {
+                                var new_feed;
+                                if (typeof(feed) === 'object') {
+                                        var link = feed.link;
+                                        if (typeof(link) === 'object') {
+                                                new_feed = link;
+                                        } else {
+                                                new_feed = feed;
+                                        }
+                                } else {
+                                        new_feed = new Object();
+                                        new_feed.link = feed;
+                                        new_feed.show = 5;
+                                        if (rss.title) {
+                                                new_feed.title = rss.title;
+                                        }
+                                }
+                                if (new_feed && new_feed.link) {
+                                        new_feeds.push(new_feed);
+                                }
+                        }
+                });
+                db.rss.channels.update({"_id" : rss._id}, { $set : { "feeds" : new_feeds}});
+        }
 });
